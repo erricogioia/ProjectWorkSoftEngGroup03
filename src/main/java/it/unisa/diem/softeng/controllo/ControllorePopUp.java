9d7802a95848f0ea83d11788dd0e29c5bf5c8422
antoniopiodamico <antoniopiodamico@macbookpro.homenet.telecomitalia.it>
2024-12-08 12:15:23 +0100

    Corpo e documentazione classi controllore

--- src/main/java/it/unisa/diem/softeng/controllo/ControllorePopUp.java
+++ src/main/java/it/unisa/diem/softeng/controllo/ControllorePopUp.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.controllo;
 
+
+import it.unisa.diem.softeng.servizio.InterfacciaRubrica;
+
 /**
- *
- * @author antoniopiodamico
- */
+ * @brief Classe ControllorePopUp gestisce la creazione delle voci della rubrica.
+ * @invariant La rubrica associata non deve essere mai null dopo l'inizializzazione del controllore.
+ * @see ControlloreVistaPrincipale
+*/
+
 public class ControllorePopUp {
     
+    private InterfacciaRubrica rubrica;
+    
+    /**
+     * @brief Metodo per la creazione di un nuovo contatto nella rubrica.
+     * @post Il contatto viene aggiunto alla rubrica, se valido.
+     * @post Il contatto non viene aggiunto alla rubrica, se non valido.
+     * @see it.unisa.diem.softeng.servizio::InterfacciaRubrica#creaContatto(Contatto)
+    */
+    public void crea() {
+        
+    }
+    
+    /**
+     * @brief Metodo per annullare l'operazione corrente.
+     * 
+     * Ripristina lo stato precedente senza salvare modifiche nella rubrica.
+     * 
+     * @pre Le diverse operazioni devono essere in corso.
+     * @post L'operazione corrente viene annullata e i dati non vengono salvati.
+    */
+    public void annulla() {
+        
+    }
+
+    private void mostraPopUpErrore(){
+        
+    }
 }
