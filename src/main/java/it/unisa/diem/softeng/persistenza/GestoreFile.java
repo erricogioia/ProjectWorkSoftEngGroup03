e302be4cb09ce2f8c43c1e4a97e842ddb4b85d99
raffaele <raffaele@localhost>
2024-12-08 12:35:20 +0100

    Corpo e documentazione classi persistenza

--- src/main/java/it/unisa/diem/softeng/persistenza/GestoreFile.java
+++ src/main/java/it/unisa/diem/softeng/persistenza/GestoreFile.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.persistenza;
 
+import it.unisa.diem.softeng.servizio.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
- *
- * @author raffaele
+ * @brief Implementa l'interfaccia InterfacciaGestoreFile per il salvataggio e il caricamento della rubrica.
+ * @see InterfacciaGestoreFile
+ * @see it.unisa.diem.softeng.servizio::InterfacciaRubrica
  */
-public class GestoreFile {
+
+public class GestoreFile implements InterfacciaGestoreFile {
+
+   /**
+     * @copydoc InterfacciaGestoreFile::esporta
+    */
+    @Override
+    public void esporta(InterfacciaRubrica rubrica) throws IOException {
+    }
     
+    /**
+     * @copydoc InterfacciaGestoreFile::importa
+    */
+    @Override
+    public InterfacciaRubrica importa() throws IOException {
+        return null;
+    }
+    
+    private void scriviFile(InterfacciaRubrica rubrica, File file) {
+
+    }
+
+    private static InterfacciaRubrica leggiFile(File file) {
+        return null;
+    }
+
 }
--- src/main/java/it/unisa/diem/softeng/persistenza/InterfacciaGestoreFile.java
+++ src/main/java/it/unisa/diem/softeng/persistenza/InterfacciaGestoreFile.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.persistenza;
 
+import it.unisa.diem.softeng.servizio.*;
+import java.io.IOException;
+
 /**
- *
- * @author raffaele
+ * @brief Interfaccia per la gestione dei file della rubrica.
  */
+
 public interface InterfacciaGestoreFile {
+    /**
+     * @brief Salva i dati della rubrica su un file.
+     * @param[in] rubrica La rubrica da salvare.
+     * @pre La rubrica non è null.
+     * @post I dati della rubrica sono salvati sul file specificato tramite il FileChooser.
+    */
+    void esporta(InterfacciaRubrica rubrica) throws IOException;
+
+    /**
+     * @brief Carica i dati di una rubrica da un file.
+     * @post La rubrica viene popolata con i dati del file specificato tramite il FileChooser.
+     * @return Un'istanza di Rubrica contenente i dati caricati.
+     */
+    InterfacciaRubrica importa() throws IOException;
     
 }
