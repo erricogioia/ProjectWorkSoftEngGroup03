5b0013ef26123847bfd173df03bfc287e7cc943d
erricogioia <e.gioia7@studenti.unisa.it>
2024-12-08 12:09:26 +0100

    Delete src/main/java directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.projectworksoftenggroup03 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.projectworksoftenggroup03 to javafx.fxml;
-    exports com.mycompany.projectworksoftenggroup03;
-}
-*/
