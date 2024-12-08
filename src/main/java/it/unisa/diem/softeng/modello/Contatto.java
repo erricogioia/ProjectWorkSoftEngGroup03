f0a6c907a3f737dc147012e597929ccd4d8325a7
mashalem <mashalem@192.168.1.17>
2024-12-08 12:29:05 +0100

    Corpo e documentazione classi modello

--- src/main/java/it/unisa/diem/softeng/modello/Contatto.java
+++ src/main/java/it/unisa/diem/softeng/modello/Contatto.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.modello;
 
+import java.util.List;
+
 /**
- *
- * @author christiandamico
+ * @brief Rappresenta un contatto nella rubrica.
+ * 
+ * Questa classe, estendendo Persona, viene usata per caratterizzare completamente un Contatto all'interno di Rubrica.
+ * 
+ * @invariant Il contatto deve avere, qualora esistano, numeri di telefono ed email validi.
+ * @invariant Il contatto deve avere massimo tre numeri di telefono.
+ * @invariant Il contatto deve avere massimo tre email.
+ * @see Persona
  */
-public class Contatto {
+public class Contatto extends Persona{
     
+    private List<String> numeriTelefono;
+    private List<String> email;
+
+    /**
+     * @brief Costruttore della classe Contatto.
+     * 
+     * Qualora venisse inserito un sottoinsieme dei parametri relativi ai numeri di telefono o alle email, la rimanente parte conterrà stringhe vuote.
+     * 
+     * @param[in] nome Il nome della Persona.
+     * @param[in] cognome Il cognome della Persona.
+     * @param[in] numeroTelefono1 Il primo numero di telefono di Contatto.
+     * @param[in] numeroTelefono2 Il secondo numero di telefono di Contatto.
+     * @param[in] numeroTelefono3 Il terzo numero di telefono di Contatto.
+     * @param[in] email1 La prima email di Contatto.
+     * @param[in] email2 La seconda email di Contatto.
+     * @param[in] email3 La terza email di Contatto.
+     */
+    public Contatto(String nome, String cognome, String numeroTelefono1, String numeroTelefono2, String numeroTelefono3, String email1, String email2, String email3) {
+        super(nome, cognome);
+    }
+
+    /**
+     * @brief Ottiene i numeri di telefono del contatto.
+     * @return Una lista di numeri di telefono.
+     */
+    public List<String> getNumeriTelefono() {
+        return null;
+    }
+
+    /**
+     * @brief Ottiene gli indirizzi email del contatto.
+     * @return Una lista di email.
+     */
+    public List<String> getEmail() {
+        return null; 
+    }
+
+    /**
+     * @brief Rappresentazione testuale del contatto.
+     * @return La rappresentazione testuale.
+     */
+    @Override
+    public String toString() {
+        return null;
+    }
 }
+
--- src/main/java/it/unisa/diem/softeng/modello/Persona.java
+++ src/main/java/it/unisa/diem/softeng/modello/Persona.java
@@ -1,14 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.modello;
 
 /**
- *
- * @author christiandamico
+ * @brief Rappresenta una persona con nome e cognome. 
+ * 
+ * Questa classe viene utilizzata come chiave per la Map di Rubrica per evitare contatti duplicati secondo equals.
+ * 
+ * @see Contatto
+ * @invariant La persona deve avere almeno un nome o un cognome valido.
  */
-public class Persona {
+public class Persona implements Comparable<Persona> {
+    
+    private String nome;
+    private String cognome;
+
+    
+    /**
+     * @brief Costruttore della classe Persona.
+     * 
+     * Qualora venisse inserito solo uno dei due parametri, l'altro conterrà una stringa vuota.
+     * 
+     * @param[in] nome Il nome della Persona.
+     * @param[in] cognome Il cognome della Persona.
+     */
+    public Persona(String nome, String cognome) {
+
+    }
+
+    /**
+     * @brief Ottiene il nome della persona.
+     * @return Il nome della persona.
+     */
+    public String getNome() {
+        return null;
+    }
+    
+    /**
+     * @brief Ottiene il cognome della persona.
+     * @return Il cognome della persona.
+    */
+    public String getCognome() {
+        return null;
+    }
+
+    /**
+     * @brief Imposta un nuovo nome per la persona.
+     * @param[in] nome Il nuovo nome della persona.
+     * @post Il campo nome è aggiornato con il nuovo valore.
+     */
+    public void setNome(String nome) {
+        
+    }
+
+    /**
+     * @brief Imposta un nuovo cognome per la persona.
+     * @param[in] cognome Il nuovo cognome della persona.
+     * @post Il campo cognome è aggiornato con il nuovo valore.
+     */
+    public void setCognome(String cognome) {
+        
+    }
+
+    /**
+     * @brief Confronta questa persona con un altro oggetto.
+     * 
+     * Il confronto avviene contemporaneamente secondo il nome e il cognome.
+     *
+     * @param[in] obj L'oggetto con cui effettuare il confronto.
+     * @return true se gli oggetti sono uguali, altrimenti false.
+    */
+    @Override
+    public boolean equals(Object obj) {
+        return true;
+    }
+
+    /**
+     * @brief Compara questa persona con un'altra.
+     * 
+     * Il confronto avviene, in ordine alfabetico, primariamente secondo il cognome e successivamente tramite il nome, rimanendo coerenti con equals.
+     * 
+     * @see equals()
+     * 
+     * @param[in] other La persona con cui effettuare il confronto.
+     * @return Un valore negativo, zero o positivo se questa persona è rispettivamente minore, uguale o maggiore, secondo il criterio summenzionato.
+    */
+    @Override
+    public int compareTo(Persona other) {
+        return 0;
+    }
+    
+    /**
+     * @brief Rappresentazione testuale della persona.
+     * @return La rappresentazione testuale.
+    */
+    @Override
+    public String toString() {
+        return null;  
+    }
     
 }
+
