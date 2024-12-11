0cd569a70693b4f7e4b9b433012090a661e31afb
mashalem <mashalem@192.168.1.17>
2024-12-11 21:44:11 +0100

    Aggiunta testCreazioneOggettoConCognomeNull

--- src/test/java/it/unisa/diem/softeng/modello/PersonaTest.java
+++ src/test/java/it/unisa/diem/softeng/modello/PersonaTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.modello;
 
 import org.junit.jupiter.api.Test;
@@ -50,6 +45,14 @@ public class PersonaTest {
     @Test
     public void testCreazioneOggettoConNomeNull() {
         assertThrows(IllegalArgumentException.class, () -> new Persona(null, "Rossi"));
+    }
+    
+    /**
+     * Test per cognome null.
+     */
+    @Test
+    public void testCreazioneOggettoConCognomeNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Persona("Mario", null));
     }
     
 }
