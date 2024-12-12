cd2d0e10eb578e42f2fbccecdb32cf00fc549297
mashalem <mashalem@192.168.1.17>
2024-12-12 22:16:37 +0100

    Aggiunta testCreazioneValida2

--- src/test/java/it/unisa/diem/softeng/modello/ContattoTest.java
+++ src/test/java/it/unisa/diem/softeng/modello/ContattoTest.java
@@ -22,4 +22,16 @@ public class ContattoTest {
         assertEquals(Arrays.asList("a@example.com", "b@example.com", "c@example.com"), c1.getEmail());
     }
     
+    /**
+     * Test del metodo di creazione con input validi.
+     */
+    @Test
+    void testCreazioneValida2() {
+        Contatto c1 = new Contatto("Mario", "Rossi", "123", "456", "", "a@example.com", "", "c@example.com");
+        assertEquals("Mario", c1.getNome());
+        assertEquals("Rossi", c1.getCognome());
+        assertEquals(Arrays.asList("123", "456", ""), c1.getNumeriTelefono());
+        assertEquals(Arrays.asList("a@example.com", "", "c@example.com"), c1.getEmail());
+    }
+    
 }
