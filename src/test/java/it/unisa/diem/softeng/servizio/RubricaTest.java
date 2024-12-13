83403b5b0e972fd0f0a69515c5831bdbb1b1b8db
antoniopiodamico <antoniopiodamico@macbookpro.homenet.telecomitalia.it>
2024-12-13 11:17:54 +0100

    Aggiunta testModificaContattoEsistente2

--- src/test/java/it/unisa/diem/softeng/servizio/RubricaTest.java
+++ src/test/java/it/unisa/diem/softeng/servizio/RubricaTest.java
@@ -105,4 +105,18 @@ public class RubricaTest {
         assertFalse(rubrica.getContatti().containsValue(c1));
     }
     
+    /**
+     * Test del metodo modificaContatto con c1 esistente. Modifica della chiave.
+     */
+    @Test
+    void testModificaContattoEsistente2() {
+        Contatto c1 = new Contatto("Mario", "Rossi", "1234567890", "1234567890", "1234567890", "a@example.com", "b@example.com", "c@example.com");
+        Persona k1 = (Persona) c1;
+        rubrica.creaContatto(c1);
+        Contatto c1Modificato = new Contatto("Stefano", "Bianchi", "1234567890", "1234567890", "1234567890", "a@example.com", "b@example.com", "c@example.com");
+        assertTrue(rubrica.modificaContatto(c1Modificato, k1));
+        assertTrue(rubrica.getContatti().containsValue(c1Modificato));
+        assertFalse(rubrica.getContatti().containsValue(c1));
+    }
+    
 }
