870472606ddba0dd6673c63e6161167ceee15625
raffaele <raffaele@localhost>
2024-12-13 12:09:22 +0100

    Aggiunta Fixtures e setUp

--- src/test/java/it/unisa/diem/softeng/servizio/ValidaContattoTest.java
+++ src/test/java/it/unisa/diem/softeng/servizio/ValidaContattoTest.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unisa.diem.softeng.servizio;
 
-import it.unisa.diem.softeng.modello.Contatto;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import it.unisa.diem.softeng.modello.Contatto;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  *
@@ -19,38 +11,11 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class ValidaContattoTest {
     
-    public ValidaContattoTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    private InterfacciaValidaContatto validatore;
     
     @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
-
-    /**
-     * Test of valida method, of class ValidaContatto.
-     */
-    @Test
-    public void testValida() {
-        System.out.println("valida");
-        Contatto contatto = null;
-        ValidaContatto instance = new ValidaContatto();
-        boolean expResult = false;
-        boolean result = instance.valida(contatto);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    void setUp() {
+        validatore = new ValidaContatto();
     }
     
 }
