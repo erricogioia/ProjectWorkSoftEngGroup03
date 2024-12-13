94a3744ef7d67fed4a5df28fda0f88e5f2119aef
raffaele <raffaele@localhost>
2024-12-13 16:04:44 +0100

    Aggiunta crea, aggiornaTabella e chiudiPopUp

--- src/main/java/it/unisa/diem/softeng/controllo/ControllorePopUp.java
+++ src/main/java/it/unisa/diem/softeng/controllo/ControllorePopUp.java
@@ -1,17 +1,49 @@
 package it.unisa.diem.softeng.controllo;
 
 
+import it.unisa.diem.softeng.modello.Contatto;
 import it.unisa.diem.softeng.servizio.InterfacciaRubrica;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * @brief Classe ControllorePopUp gestisce la creazione delle voci della rubrica.
  * @invariant La rubrica associata non deve essere mai null dopo l'inizializzazione del controllore.
  * @see ControlloreVistaPrincipale
 */
-
 public class ControllorePopUp {
     
     private InterfacciaRubrica rubrica;
+    private ObservableList<Contatto> contatti;
+    
+    @FXML
+    private TextField nomeField;
+    @FXML
+    private TextField cognomeField;
+    @FXML
+    private TextField telefono1Field;
+    @FXML
+    private TextField telefono2Field;
+    @FXML
+    private TextField telefono3Field;
+    @FXML
+    private TextField email1Field;
+    @FXML
+    private TextField email2Field;
+    @FXML
+    private TextField email3Field;
+    @FXML
+    private Button confermaButton;
+    @FXML
+    private Button annullaButton;
     
     /**
      * @brief Metodo per la creazione di un nuovo contatto nella rubrica.
@@ -19,8 +51,18 @@ public class ControllorePopUp {
      * @post Il contatto non viene aggiunto alla rubrica, se non valido.
      * @see it.unisa.diem.softeng.servizio::InterfacciaRubrica#creaContatto(Contatto)
     */
+    @FXML
     public void crea() {
+        boolean contattoValido = rubrica.creaContatto(new Contatto(nomeField.getText(), cognomeField.getText(), 
+                telefono1Field.getText(), telefono2Field.getText(), telefono3Field.getText(), 
+                email1Field.getText(), email2Field.getText(), email3Field.getText()));
         
+        if(!contattoValido)
+            mostraPopUpErrore();
+        else
+            aggiornaTabella();
+        
+        this.chiudiPopUp();
     }
     
     /**
@@ -37,5 +79,14 @@ public class ControllorePopUp {
 
     private void mostraPopUpErrore(){
         
+    }
+    
+    private void aggiornaTabella() {
+        contatti.setAll(rubrica.getContatti().values());
+    }
+    
+    private void chiudiPopUp() {
+        Stage s = (Stage) nomeField.getParent().getScene().getWindow();
+        s.close();
     }
 }
