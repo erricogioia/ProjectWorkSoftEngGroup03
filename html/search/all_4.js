035ed582a21ac0ce974a86ff4f26db63462e6ada
mashalem <mashalem@192.168.1.17>
2024-12-15 16:32:57 +0100

    Modifiche alla cartella Documents

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['importa_0',['importa',['../classit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_gestore_file.html#a5856f6f6c11993c4296ecb4d547f61a5',1,'it.unisa.diem.softeng.persistenza.GestoreFile.importa()'],['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_interfaccia_gestore_file.html#aa2fd00264e6609bf15ae3f4779f5f653',1,'it.unisa.diem.softeng.persistenza.InterfacciaGestoreFile.importa()']]],
   ['initialize_1',['initialize',['../classit_1_1unisa_1_1diem_1_1softeng_1_1controllo_1_1_controllore_pop_up.html#ad90f054f480d957c2c38293fb9ef4e03',1,'it.unisa.diem.softeng.controllo.ControllorePopUp.initialize()'],['../classit_1_1unisa_1_1diem_1_1softeng_1_1controllo_1_1_controllore_vista_principale.html#a84b05456f10ee9391c8d466831983cdf',1,'it.unisa.diem.softeng.controllo.ControlloreVistaPrincipale.initialize()']]],
-  ['interfacciagestorefile_2',['InterfacciaGestoreFile',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_interfaccia_gestore_file.html',1,'it::unisa::diem::softeng::persistenza']]],
-  ['interfacciarubrica_3',['InterfacciaRubrica',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_rubrica.html',1,'it::unisa::diem::softeng::servizio']]],
-  ['interfacciavalidacontatto_4',['InterfacciaValidaContatto',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_valida_contatto.html',1,'it::unisa::diem::softeng::servizio']]]
+  ['integrazionetest_2',['IntegrazioneTest',['../classit_1_1unisa_1_1diem_1_1softeng_1_1integrazione_1_1_integrazione_test.html',1,'it::unisa::diem::softeng::integrazione']]],
+  ['interfacciagestorefile_3',['InterfacciaGestoreFile',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_interfaccia_gestore_file.html',1,'it::unisa::diem::softeng::persistenza']]],
+  ['interfacciarubrica_4',['InterfacciaRubrica',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_rubrica.html',1,'it::unisa::diem::softeng::servizio']]],
+  ['interfacciavalidacontatto_5',['InterfacciaValidaContatto',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_valida_contatto.html',1,'it::unisa::diem::softeng::servizio']]]
 ];
--- html/search/classes_2.js
+++ html/search/classes_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['interfacciagestorefile_0',['InterfacciaGestoreFile',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_interfaccia_gestore_file.html',1,'it::unisa::diem::softeng::persistenza']]],
-  ['interfacciarubrica_1',['InterfacciaRubrica',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_rubrica.html',1,'it::unisa::diem::softeng::servizio']]],
-  ['interfacciavalidacontatto_2',['InterfacciaValidaContatto',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_valida_contatto.html',1,'it::unisa::diem::softeng::servizio']]]
+  ['integrazionetest_0',['IntegrazioneTest',['../classit_1_1unisa_1_1diem_1_1softeng_1_1integrazione_1_1_integrazione_test.html',1,'it::unisa::diem::softeng::integrazione']]],
+  ['interfacciagestorefile_1',['InterfacciaGestoreFile',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1persistenza_1_1_interfaccia_gestore_file.html',1,'it::unisa::diem::softeng::persistenza']]],
+  ['interfacciarubrica_2',['InterfacciaRubrica',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_rubrica.html',1,'it::unisa::diem::softeng::servizio']]],
+  ['interfacciavalidacontatto_3',['InterfacciaValidaContatto',['../interfaceit_1_1unisa_1_1diem_1_1softeng_1_1servizio_1_1_interfaccia_valida_contatto.html',1,'it::unisa::diem::softeng::servizio']]]
 ];
